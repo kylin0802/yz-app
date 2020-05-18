66799114559aff1f7dce6a08882dc147f832fa62
kylin <kylin0802@gmail.com>
2020-05-18 13:20:24 +0800

    首页静态页面完成

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -22,23 +22,46 @@ const Nav = (props) => {
     return (
   <div className="home-page-tag">
     <PlaceHolder title="手机开门" {...props} url="/user/addUser" />
-    <PlaceHolder title="一键小区" {...props} />
+    <PlaceHolder title="一键进小区" {...props} />
     <PlaceHolder title="信息注册" {...props}/>
-    <PlaceHolder title="信息注册" {...props}/>
-    <PlaceHolder title="信息注册" {...props}/>
-    <PlaceHolder title="信息注册" {...props} />
+    <PlaceHolder title="车辆信息" {...props}/>
+    <PlaceHolder title="家庭成员" {...props}/>
+    <PlaceHolder title="记录查询" {...props} />
   </div>
 )};
 function Home(props) {
   return (
     <React.Fragment>
+      <div className="home-page-title">
+        智慧家
+      </div>
       <section className="home-page-carousel-wrapper">
         <Carousel />
       </section>
       <section className="home-page-tag-wrapper">
-        <h5>数据</h5>
+        <h5 className="home-page-tag-title">通行统计</h5>
         <Nav {...props}/>
       </section>
+    <section className="home-page-news"><span><Icon type="exclamation-circle" /></span>名人苑智能家全面上线...</section>
+    <section className="home-page-list">
+      <h5 className="home-page-list-title">今日报警</h5>
+      <div className="home-page-list-item-wrapper">
+
+  
+      <div className="home-page-list-item">
+        <div className="home-page-list-item-title">报警时间</div>
+        <div className="home-page-list-item-content">
+          2020/03/23
+        </div>
+      </div>
+      <div className="home-page-list-item">
+      <div className="home-page-list-item-title">报警时间</div>
+        <div className="home-page-list-item-content">
+          2020/03/23
+        </div>
+      </div>
+      </div>
+    </section>
     </React.Fragment>
   );
 }
