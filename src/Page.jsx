9a63259b234234562df384b492ead3aa0558144e
你好吗筱 <52653072+haohao16@users.noreply.github.com>
2020-05-20 14:40:43 +0800

    feat: 车辆信息 添加绑定 author：hao

--- src/Page.jsx
+++ src/Page.jsx
@@ -3,6 +3,8 @@ import { Route, Switch, Redirect, HashRouter } from 'react-router-dom'; // Brows
 
 import App from './App';
 import AddUser from './containers/AddUser';
+import CarInfo from './containers/CarInfo';
+import AddFamily from './containers/AddFamily';
 // import NoFind from './containers/NoFind';
 
 export default props => {
@@ -11,6 +13,8 @@ export default props => {
       <Switch>
         {/* <Route exact path="/404" component={NoFind} /> */}
         <Route exact path="/user/addUser" component={AddUser} />
+        <Route exact path="/user/carINfo" component={CarInfo} />
+        <Route exact path="/user/addFamily" component={AddFamily} />
         <Route path="/" component={App} />
         <Redirect to="/404" />
       </Switch>
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -3,32 +3,33 @@ import Carousel from './components/Carousel';
 import { Flex, WhiteSpace, Icon } from 'antd-mobile';
 
 import './index.less';
-const PlaceHolder = (props) => {
-    const {title, url} = props
-    const handleLink = (url) => {
-        console.log("傻逼")
-        props.history.push(url)
-    }
-   return (
-  <div className="home-page-tag-item">
-    <span className="home-page-tag-item-icon" onClick={()=> handleLink(url)}>
-    <Icon type="check" />
-    </span>
-    <span className="home-page-tag-item-title">{title}</span>
-  </div>
-)};
-const Nav = (props) => {
-
-    return (
-  <div className="home-page-tag">
-    <PlaceHolder title="手机开门" {...props} url="/user/addUser" />
-    <PlaceHolder title="一键小区" {...props} />
-    <PlaceHolder title="信息注册" {...props}/>
-    <PlaceHolder title="信息注册" {...props}/>
-    <PlaceHolder title="信息注册" {...props}/>
-    <PlaceHolder title="信息注册" {...props} />
-  </div>
-)};
+const PlaceHolder = props => {
+  const { title, url } = props;
+  const handleLink = url => {
+    console.log('傻逼');
+    props.history.push(url);
+  };
+  return (
+    <div className="home-page-tag-item">
+      <span className="home-page-tag-item-icon" onClick={() => handleLink(url)}>
+        <Icon type="check" />
+      </span>
+      <span className="home-page-tag-item-title">{title}</span>
+    </div>
+  );
+};
+const Nav = props => {
+  return (
+    <div className="home-page-tag">
+      <PlaceHolder title="手机开门" {...props} url="/user/addUser" />
+      <PlaceHolder title="一键小区" {...props} />
+      <PlaceHolder title="信息注册" {...props} />
+      <PlaceHolder title="车辆信息" {...props} url="/user/CarInfo" />
+      <PlaceHolder title="添加家庭成员" {...props} url="/user/AddFamily" />
+      <PlaceHolder title="信息注册" {...props} />
+    </div>
+  );
+};
 function Home(props) {
   return (
     <React.Fragment>
@@ -37,13 +38,10 @@ function Home(props) {
       </section>
       <section className="home-page-tag-wrapper">
         <h5>数据</h5>
-        <Nav {...props}/>
+        <Nav {...props} />
       </section>
     </React.Fragment>
   );
 }
 
 export default Home;
-
-
-
